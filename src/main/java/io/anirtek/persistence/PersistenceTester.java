9d3683dfa2281e7ffc215920dbb84df7d77b49bb
anirtek <atekade1@binghamton.edu>
2018-07-24 16:14:47 -0400

    query UPDATE and DELETE BY ID

--- src/main/java/io/anirtek/persistence/PersistenceTester.java
+++ src/main/java/io/anirtek/persistence/PersistenceTester.java
@@ -10,26 +10,48 @@ public class PersistenceTester {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("anirtek-jpa");
-		
-		EntityManager em = emf.createEntityManager();
-		
-//		INSERT INTO action
-//		Employee emp = new Employee();
-//		emp.setFirstName("Mayank");
-//		emp.setLastName("Jain");
-//		emp.setEmail("mjain8@binghamton.edu");
-//		emp.setCity("San Francisco");
-//		
-//		em.getTransaction().begin();
-//		em.persist(emp);
-//		em.getTransaction().commit();
 
-//		FIND BY ID
-		Employee emp = em.find(Employee.class, "a13f3cb4-0530-4e2e-b7c0-dbfc75953426");
-		System.out.println(emp.toString());
-		
+		EntityManager em = emf.createEntityManager();
+
+		// INSERT INTO action
+		// Employee emp = new Employee();
+		// emp.setFirstName("Mayank");
+		// emp.setLastName("Jain");
+		// emp.setEmail("mjain8@binghamton.edu");
+		// emp.setCity("San Francisco");
+		//
+		// em.getTransaction().begin();
+		// em.persist(emp);
+		// em.getTransaction().commit();
+
+		// FIND BY ID
+		// Employee emp = em.find(Employee.class,
+		// "a13f3cb4-0530-4e2e-b7c0-dbfc75953426");
+		// Employee emp = em.find(Employee.class, "a13f3cb4dbfc75953426");
+		// System.out.println(emp.toString());
+
+		// UPDATE BY ID
+		// Employee emp = em.find(Employee.class,
+		// "a13f3cb4-0530-4e2e-b7c0-dbfc75953426");
+		// emp.setFirstName("Jack");
+		// emp.setLastName("Sparrow");
+		// emp.setEmail("jsparrow@binghamton.edu");
+		// emp.setCity("Los Angeles");
+		//
+		// em.getTransaction().begin();
+		// em.merge(emp);
+		// em.getTransaction().commit();
+
+		// DELETE
+		// Employee emp = em.find(Employee.class,
+		// "a13f3cb4-0530-4e2e-b7c0-dbfc75953426");
+		// if (emp != null) {
+		// em.getTransaction().begin();
+		// em.remove(emp);
+		// em.getTransaction().commit();
+		// }
+
 		em.close();
-		
-//		emf.close();
+		emf.close();
 	}
 }
